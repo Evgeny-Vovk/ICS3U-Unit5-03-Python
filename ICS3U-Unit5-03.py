1fc5382c74c2be4e0555b70a1c8c205498f52f03
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-12-07 15:40:06 +0000

    done

---
+++ ICS3U-Unit5-03.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved.
+#
+# Created by: Evgeny Vovk
+# Created on: November 2022
+# ICS3U-Unit5-02.py File, learning functions with parameters in python.
+
+
+def convert_to_mark(mark_as_level):
+
+    match mark_as_level:
+        case "4+":
+            mark_as_level = 97
+        case "4":
+            mark_as_level = 90
+        case "4-":
+            mark_as_level = 83
+        case "3+":
+            mark_as_level = 78
+        case "3":
+            mark_as_level = 74
+        case "3-":
+            mark_as_level = 71
+        case "2+":
+            mark_as_level = 68
+        case "2":
+            mark_as_level = 64
+        case "2-":
+            mark_as_level = 61
+        case "1+":
+            mark_as_level = 58
+        case "1":
+            mark_as_level = 54
+        case "1-":
+            mark_as_level = 51
+        case "R":
+            mark_as_level = 30
+        case "NE":
+            mark_as_level = 0
+        case _:
+            mark_as_level = -1
+    return mark_as_level
+
+
+def main():
+
+    mark_as_level = input("Enter the mark that you got as a level(4+, 3-, 2, R, NE): ")
+    convert_to_mark(mark_as_level)
+
+    mark_as_grade = convert_to_mark(mark_as_level)
+
+    print("")
+    if mark_as_grade <= 100 and mark_as_grade >= 0:
+        print("You got a mark of {0}%.".format(mark_as_grade))
+    elif mark_as_grade == -1:
+        print("Please input a valid mark.")
+
+    print("\n\nDone.")
+
+
+if __name__ == "__main__":
+    main()
